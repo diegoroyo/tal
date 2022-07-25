de5ce118630545c233ec11fd6d757e3305e2bffa
diegoroyo <diego.roymen@gmail.com>
2022-07-25 15:37:25 +0200

    chore: autopep8 format

--- tal/plot/xy.py
+++ tal/plot/xy.py
@@ -47,25 +47,28 @@ def plot_xy_grid(data: Union[NLOSCaptureData, NLOSCaptureData.HType],
     plt.show()
 
 # More general plotting
+
+
 def __plot_3d_interactive_axis(xyz: np.ndarray, focus_slider: np.ndarray,
-                                axis: int, title: str, slider_title: str,
-                                slider_unit: str, cmap: str = 'hot',
-                                xlabel: str = '', ylabel: str = ''):
+                               axis: int, title: str, slider_title: str,
+                               slider_unit: str, cmap: str = 'hot',
+                               xlabel: str = '', ylabel: str = ''):
     assert xyz.ndim == 3, 'Unknown datatype to plot'
     assert axis < 3, f'Data only have 3 dims (given axis={axis})'
     assert xyz.shape[axis] == len(focus_slider), \
-            'The slider and the data have different lengths'
+        'The slider and the data have different lengths'
     # Move the axis, so the interactive axis is at 0
     xyz_p = np.moveaxis(xyz, axis, 0)
-    v_min = np.min(xyz); v_max = np.max(xyz)
+    v_min = np.min(xyz)
+    v_max = np.max(xyz)
 
     # Plot the first figure
     fig = plt.figure()
     fig.suptitle(title)
 
     ax = plt.axes([0.2, 0.2, 0.7, 0.7])
-    img = ax.imshow( xyz_p[0], cmap = cmap, vmin = v_min, vmax = v_max)
-    fig.colorbar(img, ax=ax, shrink = 0.5)
+    img = ax.imshow(xyz_p[0], cmap=cmap, vmin=v_min, vmax=v_max)
+    fig.colorbar(img, ax=ax, shrink=0.5)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_xticks([])
@@ -74,13 +77,13 @@ def __plot_3d_interactive_axis(xyz: np.ndarray, focus_slider: np.ndarray,
     ax_slider = plt.axes([0.25, 0.1, 0.5, 0.03])
 
     slider = Slider(
-            ax = ax_slider,
-            label = slider_title,
-            valmin = 0,
-            valmax = len(focus_slider) - 1,
-            valinit = 0,
-            valstep = 1.0,
-            orientation='horizontal' )
+        ax=ax_slider,
+        label=slider_title,
+        valmin=0,
+        valmax=len(focus_slider) - 1,
+        valinit=0,
+        valstep=1.0,
+        orientation='horizontal')
     ax_text = plt.axes([0.25, 0.05, 0.5, 0.03])
     ax_text.axis('off')
     text = ax_text.text(0, 0, f'{focus_slider[0]} {slider_unit}')
@@ -96,7 +99,7 @@ def __plot_3d_interactive_axis(xyz: np.ndarray, focus_slider: np.ndarray,
 
 
 def plot_xy_interactive(data: Union[NLOSCaptureData, NLOSCaptureData.HType],
-                         cmap:str = 'hot'):
+                        cmap: str = 'hot'):
     if isinstance(data, NLOSCaptureData):
         assert data.H_format == HFormat.T_Sx_Sy, \
             'plot_xy_interactive does not support this data format'
@@ -107,22 +110,25 @@ def plot_xy_interactive(data: Union[NLOSCaptureData, NLOSCaptureData.HType],
             'plot_xy_interactive does not support this data format'
         txy = data
         delta_t = None
-    
+
     # Calculate time stamps
     n_t = txy.shape[0]
-    t_v = np.arange(n_t, dtype = np.float32)
+    t_v = np.arange(n_t, dtype=np.float32)
     time_unit = 'Index'
-    if delta_t is not None: t_v*=delta_t; time_unit = 'ps'
+    if delta_t is not None:
+        t_v *= delta_t
+        time_unit = 'ps'
     # Plot the data
-    return __plot_3d_interactive_axis(txy, t_v, axis = 0, 
-                                title = 'Impulse response by time',
-                                slider_title = 'Bins',
-                                slider_unit = time_unit,
-                                cmap = cmap,
-                                xlabel = 'x', ylabel = 'y')
+    return __plot_3d_interactive_axis(txy, t_v, axis=0,
+                                      title='Impulse response by time',
+                                      slider_title='Bins',
+                                      slider_unit=time_unit,
+                                      cmap=cmap,
+                                      xlabel='x', ylabel='y')
+
 
 def plot_xt_interactive(data: Union[NLOSCaptureData, NLOSCaptureData.HType],
-                         cmap:str = 'hot'):
+                        cmap: str = 'hot'):
     if isinstance(data, NLOSCaptureData):
         assert data.H_format == HFormat.T_Sx_Sy, \
             'plot_xt_interactive does not support this data format'
@@ -131,21 +137,22 @@ def plot_xt_interactive(data: Union[NLOSCaptureData, NLOSCaptureData.HType],
         assert data.ndim == 3 and data.shape[1] == data.shape[2], \
             'plot_xt_interactive does not support this data format'
         txy = data
-    
+
     # Calculate time stamps
     n_y = txy.shape[1]
-    y_v = np.arange(n_y, dtype = np.float32)
+    y_v = np.arange(n_y, dtype=np.float32)
     # Plot the data
-    return __plot_3d_interactive_axis(txy.swapaxes(0,2), y_v, axis = 1, 
-                                title = 'Impulse response by y',
-                                slider_title = 'Plane by y',
-                                slider_unit = 'Plane',
-                                cmap = cmap,
-                                xlabel = 't',
-                                ylabel = 'x')
+    return __plot_3d_interactive_axis(txy.swapaxes(0, 2), y_v, axis=1,
+                                      title='Impulse response by y',
+                                      slider_title='Plane by y',
+                                      slider_unit='Plane',
+                                      cmap=cmap,
+                                      xlabel='t',
+                                      ylabel='x')
+
 
 def plot_yt_interactive(data: Union[NLOSCaptureData, NLOSCaptureData.HType],
-                         cmap:str = 'hot'):
+                        cmap: str = 'hot'):
     if isinstance(data, NLOSCaptureData):
         assert data.H_format == HFormat.T_Sx_Sy, \
             'plot_ty_interactive does not support this data format'
@@ -154,15 +161,15 @@ def plot_yt_interactive(data: Union[NLOSCaptureData, NLOSCaptureData.HType],
         assert data.ndim == 3 and data.shape[1] == data.shape[2], \
             'plot_ty_interactive does not support this data format'
         txy = data
-    
+
     # Calculate time stamps
     n_x = txy.shape[2]
-    x_v = np.arange(n_x, dtype = np.float32)
+    x_v = np.arange(n_x, dtype=np.float32)
     # Plot the data
-    return __plot_3d_interactive_axis(txy.swapaxes(0,1), x_v, axis = 2, 
-                                title = 'Impulse response by x',
-                                slider_title = 'Plane by x',
-                                slider_unit = 'Plane',
-                                cmap = cmap,
-                                xlabel = 't',
-                                ylabel = 'y')
+    return __plot_3d_interactive_axis(txy.swapaxes(0, 1), x_v, axis=2,
+                                      title='Impulse response by x',
+                                      slider_title='Plane by x',
+                                      slider_unit='Plane',
+                                      cmap=cmap,
+                                      xlabel='t',
+                                      ylabel='y')
